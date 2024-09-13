b1bbe4c4e2dc4fb0bd4a1c5240cae3fa91b23417
Rhea Gezelle <54275927+rheagezelleramirez@users.noreply.github.com>
2024-09-13 10:12:14 +0800

    fix(auth): restrict update and create to logged-in users

--- app/Http/Requests/EmployeeUpdateRequest.php
+++ app/Http/Requests/EmployeeUpdateRequest.php
@@ -11,7 +11,7 @@ class EmployeeUpdateRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return auth()->check();
     }
 
     /**
--- app/Http/Requests/FactoryUpdateRequest.php
+++ app/Http/Requests/FactoryUpdateRequest.php
@@ -11,7 +11,7 @@ class FactoryUpdateRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return auth()->check();
     }
 
     /**
