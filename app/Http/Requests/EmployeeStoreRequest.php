1357dbd2b89059ff719b0c174f5e199bc6c1a7d1
Rhea Gezelle <54275927+rheagezelleramirez@users.noreply.github.com>
2024-09-13 09:00:32 +0800

    feat(validation): add rules for creating employee

---
+++ app/Http/Requests/EmployeeStoreRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class EmployeeStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'firstname' => ['required', 'string', 'max:255'],
+            'lastname' => ['required', 'string', 'max:255'],
+            'factory_id' => ['nullable', 'string', 'exists:factories,id'],
+            'email' => ['nullable', 'string', 'lowercase', 'email', 'max:255'],
+            'website' => ['nullable', 'string', 'max:255'],
+        ];
+    }
+}
